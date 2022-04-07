4a1896b024d4e8c042e191475afd813109f06a7f
mudit097 <85672841+mudit097@users.noreply.github.com>
2022-04-07 16:37:48 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "AWS_ACCESS_KEY" {
+  type = string
+  default = "AKIAZBBYSIORCGXXUZUE"
+} 
+variable "AWS_SECRET_KEY" {
+  type = string
+  default = "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"
+}
