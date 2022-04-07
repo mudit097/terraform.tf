40b7ddddf97929f391d5a610e43186386daa72de
mudit097 <85672841+mudit097@users.noreply.github.com>
2022-04-07 16:40:02 +0530

    Delete vars.tf

--- vars.tf
+++
@@ -1,8 +0,0 @@
-variable "AWS_ACCESS_KEY" {
-  type = string
-  default = "AKIAZBBYSIORCGXXUZUE"
-} 
-variable "AWS_SECRET_KEY" {
-  type = string
-  default = "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"
-}
